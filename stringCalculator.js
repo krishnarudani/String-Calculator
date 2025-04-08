7e6468c3b702d6fa961ba93a14c9bd5d8c876bc2
krishna rudani <krishnarudani062@gmail.com>
2025-04-08 13:58:30 +0530

    Step 3-4: Handle any amount of numbers and newline delimiters

--- stringCalculator.js
+++ stringCalculator.js
@@ -1,17 +1,18 @@
+
 function add(numbers) {
     if (numbers === "") {
         return 0;
     }
-    const nums = numbers.split(",");
+    const nums = numbers.replace(/\n/g, ',').split(",");
     return nums.reduce((sum, num) => sum + parseInt(num), 0);
 }
-
 
 console.log(add("")); // Returns 0
 console.log(add("1")); // Returns 1
 console.log(add("1,5")); // Returns 6
 console.log(add("1,2,3,4,5")); // Returns 15
-
-
+console.log(add("10,20,30,40")); // Returns 100
+console.log(add("1\n2,3")); // Returns 6
+console.log(add("1\n2\n3")); // Returns 6
 module.exports = { add };
 
--- stringCalculator.test.js
+++ stringCalculator.test.js
@@ -1,9 +1,10 @@
 const { add } = require('./stringCalculator.js');
 
-test('empty string should return 0', () => {
+test('empty string should return 0 and should handle new lines between numbers', () => {
     expect(add("")).toBe(0);
+    expect(add("1,2,3,4,5")).toBe(15);
+    expect(add("10,20,30,40")).toBe(100);
+    expect(add("1\n2,3")).toBe(6);
+    expect(add("1,2\n3")).toBe(6);
 });
 
-test('two numbers separated by comma should return their sum', () => {
-    expect(add("1,5")).toBe(6);
-});
