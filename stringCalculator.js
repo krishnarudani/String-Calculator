b9200ea137b6c062fca669273e9e87861b55b50b
krishna rudani <krishnarudani062@gmail.com>
2025-04-08 14:05:18 +0530

    Step 5: Handle negative numbers with exception

--- stringCalculator.js
+++ stringCalculator.js
@@ -4,6 +4,13 @@ function add(numbers) {
         return 0;
     }
     const nums = numbers.replace(/\n/g, ',').split(",");
+
+    // Check for negative numbers
+    const negativeNums = nums.filter(num => parseInt(num) < 0);
+    if (negativeNums.length > 0) {
+        throw new Error(`negative numbers not allowed ${negativeNums.join(',')}`);
+    }
+
     return nums.reduce((sum, num) => sum + parseInt(num), 0);
 }
 
@@ -14,5 +21,8 @@ console.log(add("1,2,3,4,5")); // Returns 15
 console.log(add("10,20,30,40")); // Returns 100
 console.log(add("1\n2,3")); // Returns 6
 console.log(add("1\n2\n3")); // Returns 6
+console.log(add("2,-4,3,-5")); // Returns not allowed -4,-5
+console.log(add("-1,2")); // Returns not allowed -1
+
 module.exports = { add };
 
--- stringCalculator.test.js
+++ stringCalculator.test.js
@@ -1,10 +1,16 @@
 const { add } = require('./stringCalculator.js');
 
-test('empty string should return 0 and should handle new lines between numbers', () => {
-    expect(add("")).toBe(0);
-    expect(add("1,2,3,4,5")).toBe(15);
-    expect(add("10,20,30,40")).toBe(100);
-    expect(add("1\n2,3")).toBe(6);
-    expect(add("1,2\n3")).toBe(6);
+test('String Calculator', () => {
+    expect(add("")).toBe(0); //empty string should return 0
+    expect(add("1,2,3,4,5")).toBe(15); //1+2+3+4+5 = 15
+    expect(add("10,20,30,40")).toBe(100); //10+20+30+40 = 100
+    expect(add("1\n2,3")).toBe(6); //1+2+3 = 6
+    expect(add("1,2\n3")).toBe(6); //1+2+3 = 6
+    expect(() => {
+        add("-1,2");
+    }).toThrow("negative numbers not allowed -1"); //negative number should throw an error
+    expect(() => {
+        add("2,-4,3,-5");
+    }).toThrow("negative numbers not allowed -4,-5"); //negative numbers should throw an error
 });
 
